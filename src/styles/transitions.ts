5ddf1eb4097bfdd850edb725b971aa4fba8af859
oyelsv <oyelsv@gmail.com>
2024-02-29 23:17:42 +0100

    feat(variables): add default transition

--- src/styles/transitions.ts
+++ src/styles/transitions.ts
@@ -8,3 +8,4 @@ export const acceleration = 'cubic-bezier(0.4, 0, 1, 1)' as const;
 export const fastOutSlowIn = 'cubic-bezier(0.4, 0, 0.2, 1)' as const;
 export const fastOutLinearIn = 'cubic-bezier(0.4, 0, 1, 1)' as const;
 export const linearOutSlowIn = 'cubic-bezier(0.0, 0, 0.2, 1)' as const;
+export const defaultTransition = '0.45s cubic-bezier(0.3, 1, 0.4, 1) 0s' as const;
